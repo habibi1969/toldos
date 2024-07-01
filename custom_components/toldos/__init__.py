290785ab182652b579a4bb57e27a43099a529f68
habibi1969 <habibi1969@gmail.com>
2024-07-01 22:21:35 +0200

    Update __init__.py

--- custom_components/toldos/__init__.py
+++ custom_components/toldos/__init__.py
@@ -1,1 +1,24 @@
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "toldos"
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the integration using YAML."""
+    _LOGGER.info("Configurando Toldos")
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the integration using UI."""
+    _LOGGER.info("Configurando Toldos desde una entrada de configuración")
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    _LOGGER.info("Descargando Toldos")
+    return True
 
