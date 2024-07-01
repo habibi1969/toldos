ac9c9bd29b2992e2e2635bafa1f526cee3529a27
habibi1969 <habibi1969@gmail.com>
2024-07-01 22:23:34 +0200

    Create sensor.py

---
+++ custom_components/toldos/sensor.py
@@ -0,0 +1,32 @@
+import logging
+from homeassistant.helpers.entity import Entity
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    async_add_entities([MiSensor()])
+
+class MiSensor(Entity):
+    def __init__(self):
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Toldo'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        """Fetch new state data for the sensor."""
+        # Aquí es donde actualizarías el estado del sensor.
+        # Por ejemplo, podrías obtener datos de una API, un dispositivo local, etc.
+        self._state = obtener_datos_del_sensor()
+
+def obtener_datos_del_sensor():
+    """Ejemplo de función para obtener datos del sensor."""
+    # Simulación de datos de sensor
+    return 42
