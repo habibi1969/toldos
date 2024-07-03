123013f34f448515098d777513deb95f8db1659b
habibi1969 <habibi1969@gmail.com>
2024-07-03 23:45:01 +0200

    Update config_flow.py

--- custom_components/toldos/config_flow.py
+++ custom_components/toldos/config_flow.py
@@ -1,5 +1,6 @@
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import callback
 from homeassistant.core import HomeAssistant
 from homeassistant.const import CONF_IP_ADDRESS
 
@@ -10,3 +11,21 @@ DATA_SCHEMA = vol.Schema(
         vol.Required(CONF_IP_ADDRESS, description={"suggested_value": "IP toldo"}): str,
     }
 )
+
+@config_entries.HANDLERS.register(DOMAIN)
+class MiIntegracionConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow para Toldos."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        if user_input is not None:
+            return self.async_create_entry(title="Toldos", data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema({
+                vol.Required("host"): str,
+                vol.Required("port"): int,
+            })
+        )
