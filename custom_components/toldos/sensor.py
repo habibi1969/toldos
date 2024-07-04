11540da148caa8752a63f35a41618e08d94f4301
habibi1969 <habibi1969@gmail.com>
2024-07-04 18:44:47 +0200

    Update sensor.py

--- custom_components/toldos/sensor.py
+++ custom_components/toldos/sensor.py
@@ -3,7 +3,7 @@ from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+async def async_setup_entry(hass, config, async_add_entities, discovery_info=None):
     async_add_entities([Toldo()])
 
 class Toldo(Entity):
