d919f1d2a606bf56aa034f5158fa65e39d963244
habibi1969 <habibi1969@gmail.com>
2024-07-04 19:03:17 +0200

    Update config_flow.py

--- custom_components/toldos/config_flow.py
+++ custom_components/toldos/config_flow.py
@@ -8,7 +8,9 @@ from .const import DOMAIN
 
 DATA_SCHEMA = vol.Schema(
     {
+        vol.Required(NAME, description={"suggested_value": "Toldo"}): str,
         vol.Required(CONF_IP_ADDRESS, description={"suggested_value": "IP toldo"}): str,
+        vol.Required("port", description={"value": "80"}): int,
     }
 )
 
@@ -19,13 +21,11 @@ class MiIntegracionConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
         if user_input is not None:
-            return self.async_create_entry(title="Toldo", data=user_input)
-
+            await self.async_set_unique_id(user_input[CONF_IP_ADDRESS])
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(title=user_input[CONF_IP_ADDRESS], data=user_input)
+        
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema({
-                vol.Required("host"): str,
-                vol.Required("port"): int,
-            })
+            step_id="user", data_schema=DATA_SCHEMA
         )
